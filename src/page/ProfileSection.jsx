a03bc168e79465955a9738a35b93637dea9ea715
Paliy <makspalij1@gmail.com>
2025-11-13 01:15:57 +0200

    Остані штрихи 3.0

--- src/page/ProfileSection.jsx
+++ src/page/ProfileSection.jsx
@@ -3,7 +3,7 @@ import '../style/ProfileSection.css';
 import profilePhoto from '../assets/Моє фото.jpg';
 import profilePhotoCR from '../assets/МоєФотоCR.jpg';
 
-function ProfileSection({selectedCategory, profileMotivation}) {
+function ProfileSection({selectedCategory}) {
     const myPhoto = selectedCategory === 'CR' ? profilePhotoCR : profilePhoto;
     
     
@@ -20,7 +20,7 @@ function ProfileSection({selectedCategory, profileMotivation}) {
                         <p>
                             Моя мотивація йти в ІЯК: Тому що в ньому я розвону скіли які уже маю, получу нові, познайомлюсь з новими людьми, Маю неймовірне бажання зловити вайб саме ІЯКу, в моїх планах бути активним і надалі в бесті
                         </p>
-                        <p>{profileMotivation}</p>
+                        <p>{myPhoto}</p>
                          </div>
                 </div>
             </section>
